b77db88a2bf35a829f73a93bbc76c54c616d5397
Terence Parr <parrt@antlr.org>
2013-06-07 18:20:47 -0700

    improve parse time graph

--- tool/playground/grammarstats/combined_time.py
+++ tool/playground/grammarstats/combined_time.py
@@ -3,45 +3,94 @@ from numpy  import *
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatch
 import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter, FuncFormatter
 
 import shared
 
 transitions_file = {}
 
-grammars = [g for g in sys.argv[1:]]
+split_index = sys.argv.index("+")
 
-files = [g+'-timings.txt' for g in grammars]
+grammars1 = [g for g in sys.argv[1:split_index]]
+grammars2 = [g for g in sys.argv[split_index+1:]]
+
+files1 = [g+'-timings.txt' for g in grammars1]
+files2 = [g+'-timings.txt' for g in grammars2]
 
 # load transition data. Each row is the list of trial stats per this one file
 
-stats = {}
 
+f, (plt1,plt2) = plt.subplots(2, sharex=True)
+# formatter = ScalarFormatter()
+# formatter.set_scientific(True)
+# formatter.set_powerlimits((-3,4))
+# plt2.yaxis.set_major_formatter(formatter)
+
+def my_formatter_fun(x, p):
+	return "%.0f" % (x / 1000.0)
+
+plt2.get_yaxis().set_major_formatter(FuncFormatter(my_formatter_fun))
+
+# 1ST GRAPH
+stats = {}
 means = {}
 nfiles = {}
-for i in range(len(grammars)):
-	g = grammars[i]
-	stats[g] = loadtxt(open(files[i],"rb"),delimiter=",",skiprows=0)
+for i in range(len(grammars1)):
+	g = grammars1[i]
+	stats[g] = loadtxt(open(files1[i],"rb"),delimiter=",",skiprows=0)
 	means[g] = [mean(filerow) for filerow in stats[g]]
 	nfiles[g] = len(stats[g]) # num files parsed for this grammar
 
-print nfiles
-
-f, (plt1,plt2) = plt.subplots(2, sharex=True)
-
 m = 0
-for g in grammars:
-	plt.plot(means[g], linewidth=0.5,
+for g in grammars1:
+	N = len(means[g])
+	plt1.plot(means[g], linewidth=0.5,
 			 color=shared.colors[g], linestyle=shared.styles[g])
-	plt.text(len(means[g])-len(g)*10, means[g][len(means[g])-1], g,
-			 fontsize=15)
+	plt1.text(N-len(g)*10, means[g][N-1], g,
+			 fontsize=15, family="serif")
 	m = max(m, max(means[g]))
 
-plt.axis(ymax=m*1.05)
+plt1.axis(ymax=m*1.08)
+
+# NOW DO 2ND GRAPH
+stats = {}
+means = {}
+nfiles = {}
+for i in range(len(grammars2)):
+	g = grammars2[i]
+	stats[g] = loadtxt(open(files2[i],"rb"),delimiter=",",skiprows=0)
+	means[g] = [mean(filerow) for filerow in stats[g]]
+	nfiles[g] = len(stats[g]) # num files parsed for this grammar
+
+m = 0
+for g in grammars2:
+	plt2.plot(means[g], linewidth=0.5,
+			 color=shared.colors[g], linestyle=shared.styles[g])
+	N = len(means[g])
+	if ( g == "C11" ):
+		plt2.text(N-len(g)*12, means[g][N-1], g,
+				  fontsize=15, family="serif")
+	elif ( g == "C11SLL" ):
+		plt2.text(N-len(g)*12, 0.40*means[g][N-1], g,
+				  fontsize=15, family="serif")
+	else:
+		plt2.text(N-len(g)*10, means[g][N-1], g,
+				  fontsize=15, family="serif")
+	m = max(m, max(means[g]))
+
+plt2.axis(ymax=m*1.08)
+
+f.subplots_adjust(hspace=0)
+plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
 
 #plt.legend(grammars,
 		   #loc='upper left' , prop={'family':'serif'})
-plt.ylabel('Parse time in ms', family="serif", size=15)
+plt1.set_ylabel('Parse time in ms', family="serif", size=15)
+plt2.set_ylabel('Parse time in 10^3 sec', family="serif", size=15)
 plt.xlabel('Files parsed', family="serif", size=15)
-#plt.savefig('parse-time-stats.pdf', format="pdf", bbox_inches='tight', pad_inches=0)
+plt.savefig('parse-time-stats.pdf',
+			format="pdf",
+			bbox_inches='tight',
+			pad_inches=0.04)
 plt.show()
 
