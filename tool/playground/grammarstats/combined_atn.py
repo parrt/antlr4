9a7f8f675075d8777d696dc0b82b95ee13c70737
Terence Parr <parrt@antlr.org>
2013-06-25 11:25:30 -0700

    record data for paper

--- tool/playground/grammarstats/combined_atn.py
+++ tool/playground/grammarstats/combined_atn.py
@@ -14,6 +14,11 @@ files = [g+'-transitions.txt' for g in grammars]
 
 # load transition data. Each row is the list of trial stats per this one file
 
+fig, ax = plt.subplots(1)
+
+fig.set_figheight(3.6)
+fig.set_figwidth(6)
+
 stats = {}
 
 means = {}
@@ -33,15 +38,15 @@ for g in grammars:
 
 	X = 20
 	Y = max(means[g][X-5:X+5])
-	if g=='Verilog2001' : Y = 0.005; X = 1
+	#if g=='Verilog2001' : Y = 0.005; X = 1
 	plt.text(X, Y, g,
 			 fontsize=15, family="serif")
 	m = max(m, max(means[g]))
 
 plt.axis(xmax=N, ymax=.5)
 
-plt.ylabel('Expected ratio of DATN to all lookahead', family="serif", size=15)
+plt.ylabel('Expected DATN simulation', family="serif", size=15)
 plt.xlabel('Files parsed', family="serif", size=15)
-plt.text(45, .4, "Curves represent 500 trials of 175 files", fontsize=15, family="serif")
+plt.text(10, .4, "Curves represent 500 trials of 175 files", fontsize=15, family="serif")
 plt.savefig('atn-decay-stats.pdf', format="pdf", bbox_inches='tight', pad_inches=0.05)
 plt.show()
