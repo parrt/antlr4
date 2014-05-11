c507863ce7130d49c830728872981171d4dc5537
Terence Parr <parrt@antlr.org>
2014-05-10 19:37:17 -0700

    better testing; all data for decision in 1 object

--- tool/test/org/antlr/v4/test/TestParserProfiler.java
+++ tool/test/org/antlr/v4/test/TestParserProfiler.java
@@ -4,6 +4,7 @@ import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.LexerInterpreter;
 import org.antlr.v4.runtime.ParserInterpreter;
+import org.antlr.v4.runtime.atn.ProfilingATNSimulator;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.tool.Grammar;
 import org.antlr.v4.tool.LexerGrammar;
@@ -12,36 +13,63 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestParserProfiler extends BaseTest {
-    @Test public void testLeftRecursiveStartRule() throws Exception {
-   		LexerGrammar lg = new LexerGrammar(
-   			"lexer grammar L;\n" +
-   			"A : 'a' ;\n" +
-   			"B : 'b' ;\n" +
-   			"C : 'c' ;\n" +
-   			"PLUS : '+' ;\n" +
-   			"MULT : '*' ;\n");
-   		Grammar g = new Grammar(
-   			"parser grammar T;\n" +
-   			"s : e ;\n" +
-   			"e : e MULT e\n" +
-   			"  | e PLUS e\n" +
-   			"  | A\n" +
-   			"  ;\n",
-   			lg);
+    LexerGrammar lg;
 
-   		testInterp(lg, g, "s", "a", 	"(s (e a))");
-   		testInterp(lg, g, "e", "a+a", 	"(e (e a) + (e a))");
-   	}
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        lg = new LexerGrammar(
+                "lexer grammar L;\n" +
+                "WS : [ \\r\\t\\n]+ -> channel(HIDDEN) ;\n" +
+                "SEMI : ';' ;\n" +
+                "DOT : '.' ;\n" +
+                "ID : [a-zA-Z]+ ;\n" +
+                "PLUS : '+' ;\n" +
+                "MULT : '*' ;\n");
+    }
 
-   	void testInterp(LexerGrammar lg, Grammar g,
-   					String startRule, String input,
-   					String parseTree)
-   	{
-   		LexerInterpreter lexEngine = lg.createLexerInterpreter(new ANTLRInputStream(input));
-   		CommonTokenStream tokens = new CommonTokenStream(lexEngine);
-   		ParserInterpreter parser = g.createParserInterpreter(tokens);
+    @Test public void testLL1() throws Exception {
+        Grammar g = new Grammar(
+                "parser grammar T;\n" +
+                "s : ';'{}\n" +
+                "  | '.'\n" +
+                "  ;\n",
+                lg);
+
+        ProfilingATNSimulator.DecisionInfo[] info = testInterp(lg, g, "s", ";");
+        assertEquals(1, info.length);
+        String expecting =
+                "{invocations=1, LLFallback=0, transitions=1, ATNTransitions=1, LLTransitions=0, lookahead=[1]}";
+        assertEquals(expecting, info[0].toString());
+        ProfilingATNSimulator.dump(info);
+    }
+
+    @Test public void testLL2() throws Exception {
+        Grammar g = new Grammar(
+                "parser grammar T;\n" +
+                "s : ID ';'{}\n" +
+                "  | ID '.'\n" +
+                "  ;\n",
+                lg);
+
+        ProfilingATNSimulator.DecisionInfo[] info = testInterp(lg, g, "s", "xyz;");
+        assertEquals(1, info.length);
+        String expecting =
+                "{invocations=1, LLFallback=0, transitions=2, ATNTransitions=2, LLTransitions=0, lookahead=[2]}";
+        assertEquals(expecting, info[0].toString());
+        ProfilingATNSimulator.dump(info);
+    }
+
+    public ProfilingATNSimulator.DecisionInfo[] testInterp(
+            LexerGrammar lg, Grammar g,
+            String startRule, String input)
+    {
+        LexerInterpreter lexEngine = lg.createLexerInterpreter(new ANTLRInputStream(input));
+        CommonTokenStream tokens = new CommonTokenStream(lexEngine);
+        ParserInterpreter parser = g.createParserInterpreter(tokens);
+        ProfilingATNSimulator profiler = new ProfilingATNSimulator(parser);
+        parser.setInterpreter(profiler);
    		ParseTree t = parser.parse(g.rules.get(startRule).index);
-   		System.out.println("parse tree: "+t.toStringTree(parser));
-   		assertEquals(parseTree, t.toStringTree(parser));
+        return profiler.getDecisionInfo();
    	}
 }
